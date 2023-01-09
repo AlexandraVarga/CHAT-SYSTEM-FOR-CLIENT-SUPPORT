c3e9416715d934ab04fae8755fff5042255387e1
Alisheikh123 <65902439+AlexandraVarga@users.noreply.github.com>
2023-01-10 01:57:25 +0500

    cha

---
+++ Frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+
+export const environment = {
+  production: false,
+  API_URL_PREFIX: 'https://localhost:44328/api',  // Online Energy Consumption Web API
+  API2_URL_PREFIX: 'https://localhost:44352/',   // Consumer Web API
+  chatHubUrl:"https://localhost:44328/chathub"
+};
+
